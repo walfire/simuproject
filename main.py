60c476bdc352dbabee7afc01a370c9d32e37bed8
Sebastian Frison <Sebastian@macbook-di-sebastian-frison.local>
2018-10-30 12:53:27 +0100

    First Layout of the Simulation App. For now, lets all work on the “Master” branch, from next week on I’ll show you how to use other branches !!!

---
+++ main.py
@@ -0,0 +1,127 @@
+#### SIMU PROJECT ####
+
+
+#### CLASSES ####
+
+#written in PSEUDOCODE
+
+
+#### NETWORK STRUCTURE ####
+
+class Agent:
+    friends = []
+    knowngames = {}
+    preferences = {}
+    #more..
+
+
+class Person(Agent):    #check on https://www.python-course.eu/python3_inheritance.php 
+    pass                   #for inherited classes
+    
+    
+class Influencer(Agent):
+    followers = []
+    
+    
+#### EFFECTS ON PREFERENCE ####
+    
+class Advertisement:
+    def effectonperson():
+        pass
+    def effectoninfluencer():
+        pass
+    #more?
+    
+class Influencersinfluence:
+    def effectonfollowers():
+        pass
+    #more?
+
+
+class Friendsinfluence:
+    def effectonfriends():
+        pass
+    #more?
+    
+
+#### GAME ####
+    
+    
+class Game:
+    decay = 0
+    popularity = 0.0
+    budget = 0.0
+    multiplayer = 0.0
+    singleplayer = 0.0
+    mainstream = 0.0
+    target = 0.0 #niche - mainstream
+    team = ["indie","blockbuster"] #optional?
+    #more?
+    
+    
+    
+#### CONVERSION ALGORITHM ####
+    
+class Conversionalgo:
+    def findpercent():
+        pass
+    def activeagent():
+        pass
+    def passiveagent():
+        pass
+    def decidegameplayed():
+        pass
+    #more?
+
+#### SIMULATION MANAGER ####
+
+class Simumanager:
+    def networkinit():      #Setup
+        pass
+    def networkfillup():
+        pass
+    def influencernetworkcreation():
+        pass
+    def setupcamesparam():
+        pass
+    def fillupknowngames():
+        pass
+    
+    def stateofknowngame():     #1 Timestamp
+        pass
+    def decay():
+        pass
+    def ad():
+        pass
+    def influ():
+        pass
+    def friend():
+        pass
+    def convalgo():
+        pass
+    
+#### DATA MANAGER ####
+
+class Datamanager:
+    def savenetwork():
+        pass
+    def savecurrenttimestamp():
+        pass
+    
+    
+#### PLOTTER ####
+        
+class plotter:
+    def setupplot():
+        pass
+    def drawnodes():
+        pass
+    def drawedges(node, depth):     #node: person & influencer, depth: how many levels of friends of friends of frieds i.e.
+        pass
+    def update():
+        pass
+    def exportplot():
+        pass
+    #more?
+    
+    
